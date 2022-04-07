0d9f09768ec42a3e735e808de3ae51561d485888
Antonio Emerson <emerson.nessa@gmail.com>
2022-04-07 11:04:53 -0300

    exercicios de logica

---
+++ aula0604.js
@@ -0,0 +1,61 @@
+// //exercicio 1
+// let nomeCompleto = "Emerson";
+// console.log(nomeCompleto)
+
+// //excercicio 2
+// let numeroA = 7;
+// let numeroB = 5;
+// let resultado = numeroA*numeroB;
+
+// console.log (resultado);
+
+// //excercicio 3
+
+// function saudacao(conteudo){
+//   return 'Oi eu sou Goku!';
+// }
+// console.log(saudacao())
+
+// //exercicio 4
+// function multiplica (n1, n2){
+//   n1 = 2, n2= 7;
+//   return n1 * n2;
+// }
+// console.log(multiplica());
+
+// //exercicio 5
+// function  podeDirgir(idade){
+//   if (idade >= 18){
+//     console.log ('Voce pode dirigir')
+//   }else{
+//     console.log('Não pode dirigir');
+//   }
+// }
+// podeDirgir (16);
+
+// //exercicio 6
+// for (let i = 0; i <= 20; i++) {
+//   console.log(i);
+// }
+
+//excercicio 7
+// for (let i = 1; i <= 20; i = i+2) {
+//   console.log(i);
+// }
+
+//exercicio 8
+// function tabuada (n1){
+//   for(let i = 1; i<=10; i++){
+//     console.log (n1*i);
+//   }
+// }
+// console.log(tabuada(3));
+
+//exercicio 9
+
+function converterEmHoras(m1){
+     return (m1 / 60 + " horas")
+}
+console.log(converterEmHoras(1200))
+
+
